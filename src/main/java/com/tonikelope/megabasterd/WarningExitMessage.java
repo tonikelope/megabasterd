2ffa2fa9df729a42d2c202231ba5575a77a8660a
Samyek <sonawanesamyek@gmail.com>
2023-01-01 10:34:38 +0000

    year update

--- src/main/java/com/tonikelope/megabasterd/WarningExitMessage.java
+++ src/main/java/com/tonikelope/megabasterd/WarningExitMessage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 tonikelope
+ * Copyright (C) 2023 tonikelope
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
